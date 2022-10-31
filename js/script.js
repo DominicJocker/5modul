2128ad14ce5fe0fcdc513daf8f0a5ac82312586a
Ihor Domnich <mr.igordomnich@gmail.com>
2022-10-31 20:03:54 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+
+
+let rateEUR = 4.78;
+let rateUSD = 4.90;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "USD":
+            result = amount / rateUSD;
+            break;
+    }
+
+    resultElement.innerText = `${amount.toFixed(2)} PLN =  ${result.toFixed(2)} ${currency}`;
+});
+
+
+
+
+
